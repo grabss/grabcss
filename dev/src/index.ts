53c3f6067831107f70246ae437fc5157dd96adc0
irohafox <matsukawa@grabss.co.jp>
2024-10-17 13:52:37 +0900

    WIP: mediaquery.scss

--- dev/src/index.ts
+++ dev/src/index.ts
@@ -1,5 +1,5 @@
-import 'the-new-css-reset/css/reset.css'
 import './styles/index.scss'
+import 'the-new-css-reset/css/reset.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
