7599ce0d8645b1897a52c2d6a5b05dec6e537917
irohafox <matsukawa@grabss.co.jp>
2024-10-17 15:36:10 +0900

    Fix: modern-compiler

--- dev/vite.config.ts
+++ dev/vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      }
+    }
+  },
   server: {
     host: '0.0.0.0',
     port: 3000
