5eba5fdd2b119744be297c2a4cf60ba6e0320183
irohafox <matsukawa@grabss.co.jp>
2024-10-17 14:22:05 +0900

    WIP: Colors

--- dev/src/index.ts
+++ dev/src/index.ts
@@ -3,6 +3,6 @@ import 'the-new-css-reset/css/reset.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>grabcss</h1>
+    <h1 class="text-primary">grabcss / Playgrounds</h1>
   </div>
 `
